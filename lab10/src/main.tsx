09a288248d8067cd840240a1442e5fd744c6bdf8
Tuan Anh Nguyen <70101967+TunNguyen09@users.noreply.github.com>
2025-11-25 21:21:33 -0500

    Delete lab10 directory

--- lab10/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Message from './Message.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Message />
-  </StrictMode>,
-)
